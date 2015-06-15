5ff0fd74f5e40d7222a8e46fcc3940c32a1f52f4
boblecodeur <tristanplouz@gmail.com>
2015-06-15 18:41:05 +0200

    Modification du script de compilation

--- client/compileAndLauch.sh
+++ client/compileAndLauch.sh
@@ -3,8 +3,12 @@ echo "Début de compilation"
 
 g++ -std=c++0x Main.cpp ./class/*.cpp -o executable `pkg-config gtkmm-2.4 --cflags --libs` -lsfml-network -lsfml-window -lsfml-system
 
-echo "Compilation terminé"
+if [ "$?" = "0" ];then
+  echo "Compilation terminé"
 
-echo "lancement exécutable"
-./executable
-echo "exécutable lancé"
+  echo "lancement exécutable"
+  ./executable
+  echo "exécutable lancé"
+else
+  echo "Erreur de compilation" 
+fi
