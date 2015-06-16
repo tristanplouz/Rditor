a94b962ab3694b7cbc63e88f8723c67d049a8502
boblecodeur <tristanplouz@gmail.com>
2015-06-16 13:44:10 +0200

    Modification et ajout de librairy

--- client/compileAndLauch.sh
+++ client/compileAndLauch.sh
@@ -1,7 +1,7 @@
 clear
 echo "Début de compilation"
 
-g++ -std=c++0x Main.cpp ./class/*.cpp -o executable `pkg-config gtkmm-2.4 --cflags --libs` -lsfml-network -lsfml-window -lsfml-system
+g++ -std=c++0x Main.cpp ./class/*.cpp -o executable `pkg-config gtkmm-3.0 gtksourceviewmm-3.0 --cflags --libs` -lsfml-network -lsfml-window -lsfml-system
 
 if [ "$?" = "0" ];then
   echo "Compilation terminé"
@@ -10,5 +10,5 @@ if [ "$?" = "0" ];then
   ./executable
   echo "exécutable lancé"
 else
-  echo "Erreur de compilation" 
+  echo "Erreur de compilation"
 fi
